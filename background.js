c7bc5053015b1617fc4a700403c52c64f1a9e99c
zolon-dev <app@zolon.net>
2026-03-20 18:09:05 +0900

    拡張機能オンオフや更新でregisterContentScriptsが消えるバグを修正

--- background.js
+++ background.js
@@ -16,8 +16,8 @@ async function handleSaveAndRegister(data, sendResponse) {
 
         // ストレージ保存処理
         // 現在のルールJSONを取得
-        const reseult = await chrome.storage.local.get(["CustoBook_Rules"]);
-        const rules = reseult.CustoBook_Rules || {}; // 新規作成だったら空リスト追加
+        const result = await chrome.storage.local.get(["CustoBook_Rules"]);
+        const rules = result.CustoBook_Rules || {}; // 新規作成だったら空リスト追加
 
         // ドメインをキーにしてデータを格納
         // 既存ドメインであれば上書き、新規なら追加
@@ -30,11 +30,8 @@ async function handleSaveAndRegister(data, sendResponse) {
 
         // ブラウザにドメイン監視を登録
         // 重複を避けるためいったん削除する
-        try {
-            await chrome.scripting.unregisterContentScripts({ ids: [`script-${domainkey}`]});
-        } catch (e) {
-            // 重複がなかった場合エラーになるが無視
-        }
+        await chrome.scripting.unregisterContentScripts({ ids: [`script-${domainkey}`]})
+            .catch(() => {}); // エラーが出ても無視
 
         // ドメイン登録処理
         await chrome.scripting.registerContentScripts([{
@@ -56,3 +53,37 @@ async function handleSaveAndRegister(data, sendResponse) {
         sendResponse({ status: "error", message: error.message});
     }
 }
+
+// 拡張機能のオンオフ、更新ボタン押下時
+chrome.runtime.onInstalled.addListener(async () => {
+    // 設定を再読込
+    const result = await chrome.storage.local.get(["CustoBook_Rules"]);
+    const rules = result.CustoBook_Rules || {}; // 新規作成だったら空リスト追加
+
+    // ループしてすべてのルールを再登録
+    for (const domainkey in rules) {
+        try {
+            // あってもなくても一旦削除
+            await chrome.scripting.unregisterContentScripts({ ids: [`script-${domainkey}`]})
+                .catch(() => {}); // エラーが出ても無視
+
+            await chrome.scripting.registerContentScripts([{
+                id: `script-${domainkey}`,
+                // httpとhttpsだけ許可
+                matches: [
+                    `https://${domainkey}/*`,
+                    `http://${domainkey}/*`
+                ],
+                js: ["content.js"], // content.jsを呼び出す(個別のルールはcontent.js内で識別)
+                runAt: "document_end" // ページ表示後に実行
+            }]);
+        } catch (e) {
+            console.log("エラー:" + e.message);
+        } // エラー無視
+    }
+});
+
+// ブラウザ起動時処理
+chrome.runtime.onStartup.addListener(() => {
+    console.log("ブラウザ起動時処理");
+});
