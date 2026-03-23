2d04a46124b1acea6265a0bd67b16fbf1ea15039
zolon-dev <app@zolon.net>
2026-03-24 00:51:08 +0900

    feat: 1サイト目仮対応(連載目次、個別エピソード、短編view実装)

--- content.js
+++ content.js
@@ -47,8 +47,6 @@
                     const htmlText = await response.text();
                     const parser = new DOMParser();
                     root = parser.parseFromString(htmlText, "text/html");
-                    console.log("root: " + root);
-
                 } catch (e) {
                     console.error("fetch失敗したので通常ドキュメント代用");
                     root = document;
@@ -58,19 +56,22 @@
                 root = document;
             }
 
+            // root中身
+            // console.log("HTML文字列：", root.documentElement.outerHTML);
+
             // タイトル
             if (currentPage.type == "index") {
                 const novelTitle = root.querySelector(currentPage.selector_title)?.innerText.trim();
                 console.log("タイトル: " + novelTitle);
                 const novelAuthor = root.querySelector(currentPage.selector_author)?.innerText.trim();
-                console.log("著者: " + novelAuthor); // 「作者：」を汎用処理を維持しつつ抜くには？
+                console.log("著者: " + novelAuthor); // TODO: 「作者：」を汎用処理を維持しつつ抜く
                 const novelSummary = root.querySelector(currentPage.selector_summary)?.innerText.trim();
                 console.log("あらすじ: " + novelSummary);
 
                 // 目次の箱を押さえる
+                const episodes = []; // 格納変数
                 if (currentPage.selector_episode_list) {
                     const episodeNodes = root.querySelectorAll(currentPage.selector_episode_list);
-                    const episodes = []; // 格納変数
 
                     // 箱から個別にタイトルやリンク、日付を取り出す
                     episodeNodes.forEach((node, index) => {
@@ -88,7 +89,7 @@
                     });
 
                     // とりあえず出力
-                    console.table(episodes);
+                    // console.table(episodes);
                 }
 
                 console.log("[CustoBook] UI構築開始");
@@ -97,14 +98,37 @@
                 const uiRoot = document.createElement("div");
                 uiRoot.id = "custobook-root";
 
-                // テスト文字
-                uiRoot.innerHTML = `<h1 style="font-size: 32px; margin: 0;">CustoBook</h1>`;
+                // 目次の行作成
+                const episodeLink = episodes.map(ep => `
+                    <tr class="cb-episode-list-row">
+                        <td class="cb-episode-list-title">
+                            <a href="${ep.url}" class="cb-episode-list-link">${ep.title}</a>
+                        </td>
+                    </tr>
+                `).join('');
+
+                // 流し込み
+                uiRoot.innerHTML = `
+                    <div class="cb-container">
+                        <h1 class="cb-title">${novelTitle || 'タイトル不明'}</h1>
+                        <p class="cb-author">${novelAuthor || '著者不明'}</p>
+
+                        <div class="cb-summary">${novelSummary || 'あらすじ不明'}</div>
+
+                        <table class="cb-episode-list">
+                            ${episodeLink}
+                        </table>
+                               
+                        <footer style="margin-top: 100px; padding-bottom: 10px; text-align: center; color: #ffffff;">
+                            CustoBook v0.x.x - ESC to exit
+                        </footer>
+                    </div>`;
 
                 // ブラウザに挿入
                 document.body.appendChild(uiRoot);
                 console.log("[CustoBook] UI構築完了");
             } else if (currentPage.type == "episode") {
-                const subTitle = root.querySelector(currentPage.selector_subtitle)?.innerText.trim();
+                const subTitle = root.querySelector(currentPage.selector_subtitle)?.textContent.trim();
                 console.log("サブタイトル: " + subTitle);
 
                 // Pタグ除去関数
@@ -131,15 +155,50 @@
                 const afterwordText = getLines(currentPage.selector_content_afterword);
                 if (afterwordText != "") { console.log("後書き: " + afterwordText); }
 
+                console.log("[CustoBook] UI構築開始");
+                
+                // UIの土台
+                const uiRoot = document.createElement("div");
+                uiRoot.id = "custobook-root";
+
+                let htmlContent = "";
+                htmlContent += `<div class="cb-container">`;
+                htmlContent += `<h1 class="cb-subtitle">${subTitle || 'タイトル不明'}</h1>`;
+
+                // 前書き
+                if (prefaceText) {
+                    htmlContent += `<div class="cb-preface">${prefaceText.replace(/\n/g, '<br />')}</div><hr class="cb-separator">`;
+                }
+
+                // 本文
+                htmlContent += `<div class="cb-body">${bodyText.replace(/\n/g, '<br />')}</div>`; // 本文は必ずある
+
+                // 後書き
+                if (afterwordText) {
+                    htmlContent += `<hr class="cb-separator"><div class="cb-afterword">${afterwordText.replace(/\n/g, '<br />')}</div>`;
+                }
+
+                htmlContent += `
+                    <footer style="margin-top: 100px; padding-bottom: 10px; text-align: center; color: #ffffff;">
+                        CustoBook v0.x.x - ESC to exit
+                    </footer>`;
+                htmlContent += `</div>`;
+                
+                // ブラウザに挿入
+                uiRoot.innerHTML = htmlContent;
+                document.body.appendChild(uiRoot);
+
+                console.log("[CustoBook] UI構築完了");
+
                 // 現在の話数
                 const epNumber = root.querySelector(currentPage.selector_number)?.innerText.trim();
                 console.log("話数: " + epNumber);  
             } else if (currentPage.type == "short_story") {
-                const novelTitle = root.querySelector(currentPage.selector_title)?.innerText.trim();
-                console.log("タイトル: " + novelTitle);
+                const novelTitle = root.querySelector(currentPage.selector_title)?.textContent.trim();
+                // console.log("タイトル: " + novelTitle);
 
-                const novelAuthor = root.querySelector(currentPage.selector_author)?.innerText.trim();
-                console.log("著者: " + novelAuthor);
+                const novelAuthor = root.querySelector(currentPage.selector_author)?.textContent.trim();
+                console.log("著者: " + novelAuthor.replace(/\n/g, ''));
 
                 // Pタグ除去関数
                 const getLines = (selector) => {
@@ -155,15 +214,51 @@
 
                 // 前書き
                 const prefaceText = getLines(currentPage.selector_content_preface);
-                if (prefaceText != "") { console.log("前書き: " + prefaceText.substring(0, 100)); }
+                // if (prefaceText != "") { console.log("前書き: " + prefaceText.substring(0, 100)); }
 
                 // 本文
                 const bodyText = getLines(currentPage.selector_content) || "本文なし";
-                console.log("本文: " + bodyText.substring(0, 300));
+                // console.log("本文: " + bodyText.substring(0, 300));
 
                 // 後書き
                 const afterwordText = getLines(currentPage.selector_content_afterword);
-                if (afterwordText != "") { console.log("後書き: " + afterwordText.substring(0, 100)); }
+                // if (afterwordText != "") { console.log("後書き: " + afterwordText.substring(0, 100)); }
+
+                console.log("[CustoBook] UI構築開始");
+
+                // UIの土台
+                const uiRoot = document.createElement("div");
+                uiRoot.id = "custobook-root";
+
+                let htmlContent = "";
+                htmlContent += `<div class="cb-container">`;
+                htmlContent += `<h1 class="cb-title">${novelTitle || 'タイトル不明'}</h1>`;
+                htmlContent += `<p class="cb-author">${novelAuthor || '著者不明'}</p>`;
+
+                // 前書き
+                if (prefaceText) {
+                    htmlContent += `<div class="cb-preface">${prefaceText.replace(/\n/g, '<br />')}</div><hr class="cb-separator">`;
+                }
+
+                // 本文
+                htmlContent += `<div class="cb-body">${bodyText.replace(/\n/g, '<br />')}</div>`; // 本文は必ずある
+
+                // 後書き
+                if (afterwordText) {
+                    htmlContent += `<hr class="cb-separator"><div class="cb-afterword">${afterwordText.replace(/\n/g, '<br />')}</div>`;
+                }
+
+                htmlContent += `
+                    <footer style="margin-top: 100px; padding-bottom: 10px; text-align: center; color: #ffffff;">
+                        CustoBook v0.x.x - ESC to exit
+                    </footer>`;
+                htmlContent += `</div>`;
+
+                // ブラウザに挿入
+                uiRoot.innerHTML = htmlContent;
+                document.body.appendChild(uiRoot);
+
+                console.log("[CustoBook] UI構築完了");
             }
         }
 
